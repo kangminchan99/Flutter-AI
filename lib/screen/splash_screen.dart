f44376aa9576af6b669387d1d018a7a2a1cc686a
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2024-02-15 19:18:18 +0900

    2/ 15

---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,16 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+// class MySplashScreen extends StatefulWidget {
+//   const MySplashScreen({super.key});
+
+//   @override
+//   State<MySplashScreen> createState() => _MySplashScreenState();
+// }
+
+// class _MySplashScreenState extends State<MySplashScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return FlutterNativeSplash();
+//   }
+// }
